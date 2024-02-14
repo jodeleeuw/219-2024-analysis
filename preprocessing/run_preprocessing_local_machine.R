48e1237f068d6639889ac209db3a3ee1f8a9d4be
Josh de Leeuw <josh.deleeuw@gmail.com>
2024-02-14 13:49:27 -0500

    typo fix

--- preprocessing/run_preprocessing_local_machine.R
+++ preprocessing/run_preprocessing_local_machine.R
@@ -2,7 +2,8 @@ library(readr)
 library(stringr)
 source("preprocessing/preprocessing-eeg-functions.R")
 
-subjects <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14")
+subjects <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14",
+              "16", "17", "18", "20")
 
 for(subject in subjects){
   
