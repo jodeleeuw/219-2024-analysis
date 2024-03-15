c571a92a21e01151a8183912213acf62a097a8fc
Josh de Leeuw <josh.deleeuw@gmail.com>
2024-03-15 11:06:52 -0400

    first draft of a complete analysis

--- preprocessing/merge-behavioral-local.R
+++ preprocessing/merge-behavioral-local.R
@@ -1,9 +1,20 @@
 library(jsonlite)
 library(dplyr)
 library(readr)
+library(tidyr)
 
 behavior.files <- list.files('data/raw/beh', pattern="json", full.names = T)
 
 merged.data <- lapply(behavior.files, fromJSON) %>% bind_rows()
+
+response_column <- merged.data$response
+
+null_positions <- which(sapply(response_column, is.null))
+
+new_values <- rep(NA, length(response_column))
+
+new_values[-null_positions] <- unlist(response_column, use.names = F)
+
+merged.data$response <- new_values
 
 write_csv(merged.data, file="data/final/behavioral.csv")
