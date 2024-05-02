eb83b92adcb09147887f24f0e6361b2f2d3a99a8
Josh de Leeuw <josh.deleeuw@gmail.com>
2024-05-02 17:09:48 -0400

    use phase 1 folder

--- preprocessing/download-raw-data-from-osf.R
+++ preprocessing/download-raw-data-from-osf.R
@@ -3,11 +3,11 @@ library(dplyr)
 
 osf_retrieve_node("v7mbj") %>%
   osf_ls_files(path="Behavioral", n_max=Inf) %>%
-  osf_download(path="data/raw/beh", conflicts="skip")
+  osf_download(path="data/phase_1/raw/beh", conflicts="skip")
 
 osf_retrieve_node("v7mbj") %>%
   osf_ls_files(path="EEG", n_max=Inf) %>%
-  osf_download(path="data/raw/eeg", conflicts="skip")
+  osf_download(path="data/phase_1/raw/eeg", conflicts="skip")
 
 
 # phase 2
