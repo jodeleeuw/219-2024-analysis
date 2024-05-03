49df433f063c24c08919f57638392ada7bd59f98
Josh de Leeuw <josh.deleeuw@gmail.com>
2024-05-02 22:16:59 -0400

    get only phase 2

--- preprocessing/download-raw-data-from-osf.R
+++ preprocessing/download-raw-data-from-osf.R
@@ -1,6 +1,10 @@
 library(osfr)
 library(dplyr)
 
+get_phase_1 <- FALSE
+get_phase_2 <- TRUE
+
+if(get_phase_1){
 osf_retrieve_node("v7mbj") %>%
   osf_ls_files(path="Behavioral", n_max=Inf) %>%
   osf_download(path="data/phase_1/raw/beh", conflicts="skip")
@@ -8,10 +12,10 @@ osf_retrieve_node("v7mbj") %>%
 osf_retrieve_node("v7mbj") %>%
   osf_ls_files(path="EEG", n_max=Inf) %>%
   osf_download(path="data/phase_1/raw/eeg", conflicts="skip")
-
+}
 
 # phase 2
-
+if(get_phase_2){
 osf_retrieve_node("gd24h") %>%
   osf_ls_files(path="Behavioral", n_max=Inf) %>%
   osf_download(path="data/phase_2/raw/beh", conflicts="skip")
@@ -19,3 +23,4 @@ osf_retrieve_node("gd24h") %>%
 osf_retrieve_node("gd24h") %>%
   osf_ls_files(path="EEG", n_max=Inf) %>%
   osf_download(path="data/phase_2/raw/eeg", conflicts="skip")
+}
