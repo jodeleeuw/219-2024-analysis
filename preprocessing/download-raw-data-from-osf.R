653d08a03b3305291e50594bfbd9d638727d0b5f
Josh de Leeuw <josh.deleeuw@gmail.com>
2024-05-02 21:17:12 -0400

    2 files to speed up debugging

--- preprocessing/download-raw-data-from-osf.R
+++ preprocessing/download-raw-data-from-osf.R
@@ -2,20 +2,20 @@ library(osfr)
 library(dplyr)
 
 osf_retrieve_node("v7mbj") %>%
-  osf_ls_files(path="Behavioral", n_max=Inf) %>%
+  osf_ls_files(path="Behavioral", n_max=2) %>%
   osf_download(path="data/phase_1/raw/beh", conflicts="skip")
 
 osf_retrieve_node("v7mbj") %>%
-  osf_ls_files(path="EEG", n_max=Inf) %>%
+  osf_ls_files(path="EEG", n_max=2) %>%
   osf_download(path="data/phase_1/raw/eeg", conflicts="skip")
 
 
 # phase 2
 
 osf_retrieve_node("gd24h") %>%
-  osf_ls_files(path="Behavioral", n_max=Inf) %>%
+  osf_ls_files(path="Behavioral", n_max=2) %>%
   osf_download(path="data/phase_2/raw/beh", conflicts="skip")
 
 osf_retrieve_node("gd24h") %>%
-  osf_ls_files(path="EEG", n_max=Inf) %>%
+  osf_ls_files(path="EEG", n_max=2) %>%
   osf_download(path="data/phase_2/raw/eeg", conflicts="skip")
