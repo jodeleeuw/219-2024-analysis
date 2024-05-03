09da3d542137e6dc65607e68a435ba40ed25eba6
Josh de Leeuw <josh.deleeuw@gmail.com>
2024-05-03 08:39:49 -0400

    revert to 2 for testing

--- preprocessing/download-raw-data-from-osf.R
+++ preprocessing/download-raw-data-from-osf.R
@@ -17,10 +17,10 @@ osf_retrieve_node("v7mbj") %>%
 # phase 2
 if(get_phase_2){
 osf_retrieve_node("gd24h") %>%
-  osf_ls_files(path="Behavioral", n_max=Inf) %>%
+  osf_ls_files(path="Behavioral", n_max=2) %>%
   osf_download(path="data/phase_2/raw/beh", conflicts="skip")
 
 osf_retrieve_node("gd24h") %>%
-  osf_ls_files(path="EEG", n_max=Inf) %>%
+  osf_ls_files(path="EEG", n_max=2) %>%
   osf_download(path="data/phase_2/raw/eeg", conflicts="skip")
 }
